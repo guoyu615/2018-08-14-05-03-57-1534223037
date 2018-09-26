60b0942425833ca904dc12998d370c3113995bb9
guoyu615 <guoyu0615@hotmail.com>
2018-09-27 00:12:35 +0800

    add my answer

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(s) {
   // Write your code here
+  var x = s.split("");   
+  var sum = 0;
+  for (i = 0; i < x.length; i++) { 
+     sum += parseInt(x[i]);
+     };
+	 return sum
 };
